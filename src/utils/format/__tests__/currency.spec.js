f89b97a7d9b14349c7872fccb0653a246f9ae4a2
TimPrachasri <nantawat.prac@gmail.com>
2019-09-09 15:11:43 +0700

    test: move to table test

--- src/utils/format/__tests__/currency.spec.js
+++ src/utils/format/__tests__/currency.spec.js
@@ -1,28 +1,34 @@
 import { numberToCurrencyString, currencyToNumberString } from '@/utils/format'
 
 describe('numberToCurrencyString', () => {
-  it('should create a correct currency string', () => {
-    expect(numberToCurrencyString(0.112)).toBe('0.11')
-    expect(numberToCurrencyString(1.0)).toBe('1.00')
-    expect(numberToCurrencyString(1.01)).toBe('1.01')
-    expect(numberToCurrencyString(1.012)).toBe('1.01')
-    expect(numberToCurrencyString(1.112)).toBe('1.11')
-    expect(numberToCurrencyString(10.112)).toBe('10.11')
-    expect(numberToCurrencyString(1000.112)).toBe('1,000.11')
-    expect(numberToCurrencyString(1.0, 4)).toBe('1.00')
-    expect(numberToCurrencyString(1.01, 4)).toBe('1.01')
-    expect(numberToCurrencyString(1.0111, 4)).toBe('1.0111')
-    expect(numberToCurrencyString(1.0111, 4)).toBe('1.0111')
-    expect(numberToCurrencyString(10.0111, 4)).toBe('10.0111')
-    expect(numberToCurrencyString(1000.0111, 4)).toBe('1,000.0111')
-    expect(numberToCurrencyString(1000.1111, 4)).toBe('1,000.1111')
-  })
+  test.each([
+    [0.112, undefined, '0.11'],
+    [1.0, undefined, '1.00'],
+    [1.01, undefined, '1.01'],
+    [1.012, undefined, '1.01'],
+    [1.112, undefined, '1.11'],
+    [10.112, undefined, '10.11'],
+    [1000.112, undefined, '1,000.11'],
+    [1.0, 4, '1.00'],
+    [1.01, 4, '1.01'],
+    [1.0111, 4, '1.0111'],
+    [10.0111, 4, '10.0111'],
+    [1000.0111, 4, '1,000.0111'],
+    [1000.1111, 4, '1,000.1111'],
+  ])(
+    'should create a correct currency string',
+    (amount, decimals, expected) => {
+      expect(numberToCurrencyString(amount, decimals)).toBe(expected)
+    },
+  )
 })
 
 describe('currencyToNumberString', () => {
-  it('should create a correct number string', () => {
-    expect(currencyToNumberString('1,000')).toBe('1000')
-    expect(currencyToNumberString('10,000,000')).toBe('10000000')
-    expect(currencyToNumberString('10,000,000.0122')).toBe('10000000.0122')
+  test.each([
+    ['1,000', '1000'],
+    ['10,000,000', '10000000'],
+    ['10,000,000.0122', '10000000.0122'],
+  ])('should create a correct number string', (currencyStr, expected) => {
+    expect(currencyToNumberString(currencyStr)).toBe(expected)
   })
 })
